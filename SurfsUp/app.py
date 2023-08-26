9eb9e55a6ba66ab9dbe534fdaf642da56fccd07f
Ayan Ajanaku <130423493+aajanaku@users.noreply.github.com>
2023-08-26 17:05:23 -0500

    finish starter and add app

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,67 @@
 # Import the dependencies.
-
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine, func, MetaData
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+metadata = MetaData()
 
+# Reflect the tables from the database into metadata
+metadata.reflect(bind=engine)
 # reflect the tables
+Base = automap_base()
+
+# Reflect the tables from the database into the base
+Base.prepare(engine, reflect=True)
 
 
 # Save references to each table
-
+Station = Base.classes.station
+Measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+Session = sessionmaker(bind=engine)
+session = Session()
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def Welcome():
+    return(f"""
+Welcome to my climate dashboard<br/>          
+Available Routes:<br/>
+           /api/v1.0/precipitation<br/>           
+""")
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    rows = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prev_year).all()
+    session.close()
+    precip = {date: prcp for date, prcp in rows}
+    return jsonify(precip)
+
+
+if __name__ == "__main__":
+    app.run()
